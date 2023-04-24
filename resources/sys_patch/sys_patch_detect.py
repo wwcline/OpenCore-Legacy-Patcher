cfc1876c48d8501839cfc455b0af53b094ebffbb
Mykola Grymalyuk <khronokernel@icloud.com>
2023-04-24 16:25:56 -0600

    sys_patch_detect.py: Fix AMFIPass detection

--- resources/sys_patch/sys_patch_detect.py
+++ resources/sys_patch/sys_patch_detect.py
@@ -609,7 +609,7 @@ class DetectRootPatch:
         if self.constants.detected_os < os_data.os_data.big_sur:
             return amfi_detect.AmfiConfigDetectLevel.NO_CHECK
 
-        if not utilities.check_kext_loaded("AMFIPass"):
+        if utilities.check_kext_loaded("AMFIPass"):
             # If AMFIPass is loaded, our binaries will work
             return amfi_detect.AmfiConfigDetectLevel.NO_CHECK
 
